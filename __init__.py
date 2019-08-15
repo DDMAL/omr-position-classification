e140813d466ec76bb70a918b2b6765abe0e0c677
Evan Savage <evansavage@Evans-Mac-mini.local>
2019-08-15 16:07:36 -0400

    Added __init__.py, loading position-classification module

---
+++ __init__.py
@@ -0,0 +1,9 @@
+import rodan
+__version__ = "1.0.0"
+
+import logging
+logger = logging.getLogger('rodan')
+
+from rodan.jobs import module_loader
+
+module_loader('rodan.jobs.omr-position-classification.position-classification')
