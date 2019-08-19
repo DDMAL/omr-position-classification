ddb736b0e08c082a16468feed3895482033596d9
Evan Savage <evansavage@Evans-Mac-mini.local>
2019-08-19 12:44:16 -0400

    Reformatted data types for model's predicted type

--- model_processing.py
+++ model_processing.py
@@ -8,6 +8,7 @@ from keras.backend import image_data_format
 def process_neumes(image, coords, avg_neume_height, model_path):
 
     labels = []
+    bounding_boxes = []
 
     model = load_model(model_path)
 
@@ -18,7 +19,14 @@ def process_neumes(image, coords, avg_neume_height, model_path):
             c[0]-2*avg_neume_height:c[0]+c[2]+2*avg_neume_height,
             c[1]:c[1]+c[3]]
         resize = cv.resize(bounding_box, (30,120), interpolation = cv.INTER_AREA)
-        prediction = model.predict(resize)
+        resize = resize / 255.0
+        bounding_boxes.append(resize)
+
+    bounding_boxes = [(bb / 1) for bb in bounding_boxes]
+    bounding_boxes = np.asarray(bounding_boxes).reshape(len(bounding_boxes),120,30,3)
+
+    for bb in bounding_boxes:
+        prediction = model.predict(bb)
         label = np.argmax(prediction)
         labels.append(label)
 
