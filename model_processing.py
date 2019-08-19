9c00c10f01aa59fab4fc332fb90c86d95d76c5e7
Evan Savage <evansavage@Evans-Mac-mini.local>
2019-08-19 10:16:22 -0400

    Added model_processing file

---
+++ model_processing.py
@@ -0,0 +1,14 @@
+import keras
+import cv2
+import numpy as np
+
+from keras.models import load_model
+from keras.backend import image_data_format
+
+def process_neumes(image, coords, model):
+
+    labels = ['1', '2', '3']
+
+
+
+    return labels
