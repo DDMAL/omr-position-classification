ff49961afa35c4547aa341f07b30b1a97cac7b9e
Evan Savage <evansavage@Evans-Mac-mini.local>
2019-08-19 11:47:47 -0400

    Added to process_neumes method, resizing image for model prediction

--- model_processing.py
+++ model_processing.py
@@ -1,14 +1,26 @@
 import keras
-import cv2
+import cv2 as cv
 import numpy as np
 
 from keras.models import load_model
 from keras.backend import image_data_format
 
-def process_neumes(image, coords, model):
+def process_neumes(image, coords, avg_neume_height, model_path):
 
-    labels = ['1', '2', '3']
+    labels = []
 
+    model = load_model(model_path)
+
+    [height, width, channels] = image.shape
+
+    for c in coords:
+        bounding_box = image[
+            c[0]-2*avg_neume_height:c[0]+c[2]+2*avg_neume_height,
+            c[1]:c[1]+c[3]]
+        resize = cv.resize(bounding_box, (30,120), interpolation = cv.INTER_AREA)
+        prediction = model.predict(resize)
+        label = np.argmax(prediction)
+        labels.append(label)
 
 
     return labels
