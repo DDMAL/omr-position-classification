c56aa2aa728a4da614f6e5701e8b3ff4d13b1227
Evan Savage <evansavage@Evans-Mac-mini.local>
2019-08-20 16:19:34 -0400

    Modularized output xml write script

--- xml_update.py
+++ xml_update.py
@@ -21,3 +21,29 @@ def get_glyph_coords(xml_path):
     avg_glyph_height = int(glyph_height_sum/glyph_count)
 
     return glyph_coords, avg_glyph_height
+
+
+def write_output_xml(input_xml_path, output_xml_path, predictions):
+
+    with open(input_xml_path, 'r') as in_file:
+        buf = in_file.readlines()
+
+    inc = 0
+
+    with open(output_xml_path, 'w') as out_file:
+        for line in buf:
+            if "</ids>" in line:
+                position = str(labels[np.argmax(predictions[inc])])
+                confidence = str(round(max(predictions[inc]) * 100, 2))
+                line = line + \
+                    '\t\t\t<type name=""/>\n' + \
+                    '\t\t\t<pitch-estimation>\n' + \
+                    '\t\t\t\t<position name="' + position + \
+                    '" confidence="' + confidence + '"/>\n' + \
+                    '\t\t\t\t<pitch name=""/>\n' + \
+                    '\t\t\t</pitch-estimation>\n'
+                inc += 1
+            out_file.write(line)
+        out_file.close()
+
+    return True
