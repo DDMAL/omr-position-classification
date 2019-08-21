669652629ed67982c07e617122697c65d2ba1fff
Evan Savage <evansavage@Evans-Mac-mini.local>
2019-08-21 12:01:29 -0400

    Added new training task

---
+++ position_training.py
@@ -0,0 +1,54 @@
+#-----------------------------------------------------------------------------
+# Program Name:         position_classification.py
+# Program Description:  Rodan wrapper for position classification
+#-----------------------------------------------------------------------------
+
+import numpy as np
+import cv2 as cv
+import fileinput
+
+from rodan.jobs.base import RodanTask
+
+class PositionTraining(RodanTask):
+    name = 'Position Training'
+    author = 'Evan Savage'
+    description = 'With original manuscript image and bounding box gamera xml files as inputs, individually label each neume component label to use for model training'
+    enabled = True
+    category = 'OMR'
+    interactive = True
+
+    settings = {
+        'job_queue': 'Python3'
+    }
+
+    input_port_types = (
+        {'name': 'Original Image', 'minimum': 1, 'maximum': 1, 'resource_types': lambda mime: mime.startswith('image/')},
+        {'name': 'GameraXML File', 'minimum': 1, 'maximum': 1, 'resource_types': ['application/gamera+xml']},
+        # {'name': 'Position Model', 'minimum': 1, 'maximum': 1, 'resource_types': ['keras/model+h5']},
+    )
+
+    output_port_types = (
+        {'name': 'Trained Position Model', 'minimum': 1, 'maximum': 1, 'resource_types': ['keras/model+h5']},
+    )
+
+    def get_my_interface(self, inputs, settings):
+
+        data = {
+            'title': Yeet
+        }
+
+        return 'interfaces/position_training.html', data
+
+    def run_my_task(self, inputs, settings, outputs):
+
+        input_position_model_path = inputs['Position Model'][0]['resource_path']
+        input_xml_path = inputs['GameraXML File'][0]['resource_path']
+        input_img_path = inputs['Original Image'][0]['resource_path']
+
+        image = cv.imread(input_img_path, True)
+
+        output_position_model_path = outputs['Trained Position Model'][0]['resource_path']
+
+
+
+        return True
