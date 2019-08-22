8c7e648a64311b2bd1ed3d7f7ef72b75d04181d6
Evan Savage <evansavage@Evans-Mac-mini.local>
2019-08-22 16:44:25 -0400

    Added training_interface function file

---
+++ training_interface.py
@@ -0,0 +1,5 @@
+from rodan.settings import MEDIA_URL, MEDIA_ROOT
+
+def media_file_path_to_public_url(media_file_path):
+    chars_to_remove = len(MEDIA_ROOT)
+    return os.path.join(MEDIA_URL, media_file_path[chars_to_remove:])
