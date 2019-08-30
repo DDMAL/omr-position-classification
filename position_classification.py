d8441d8ed1df698ad0242c932fc9792c96e75a6e
evansavage <esav678@gmail.com>
2019-08-30 16:56:02 -0400

    Added staff layer bounding box logic

--- position_classification.py
+++ position_classification.py
@@ -29,6 +29,7 @@ class PositionClassification(RodanTask):
         {'name': 'GameraXML File', 'minimum': 1, 'maximum': 1, 'resource_types': ['application/gamera+xml']},
         {'name': 'Position Model', 'minimum': 1, 'maximum': 1, 'resource_types': ['keras/model+hdf5']},
         {'name': 'Staff Detection Model', 'minimum': 1, 'maximum': 1, 'resource_types': ['tensorflow/frozen+inference+graph+pb']},
+        {'name': 'Staff Layer Image', 'minimum': 1, 'maximum': 1, 'resource_types': lambda mime: mime.startswith('image/')},
         {'name': 'Type Model', 'minimum': 0, 'maximum': 1, 'resource_types': ['keras/model+hdf5']},
     )
 
@@ -42,6 +43,7 @@ class PositionClassification(RodanTask):
         input_staff_detect_path = inputs['Staff Detection Model'][0]['resource_path']
         input_xml_path = inputs['GameraXML File'][0]['resource_path']
         input_img_path = inputs['Original Image'][0]['resource_path']
+        input_staff_layer_path = inputs['Staff Layer Image'][0]['resource_path']
 
         input_type_model_path = ''
 
@@ -50,9 +52,14 @@ class PositionClassification(RodanTask):
 
         image = cv.imread(input_img_path, True)
 
+        height,width,channels = image.shape
+
         output_xml_path = outputs['Extended GameraXML File'][0]['resource_path']
 
+        staff_bb = processing.get_staff_bounding_boxes(input_staff_layer_path, input_staff_detect_path)
+
         glyph_coords, avg_glyph_height = xml_update.get_glyph_coords(input_xml_path)
+
 
         pos_predictions, type_predictions = processing.process_neumes(
             image,
@@ -61,6 +68,9 @@ class PositionClassification(RodanTask):
             input_position_model_path,
             input_type_model_path)
 
-        xml_update.write_classification_xml(input_xml_path, output_xml_path, pos_predictions, type_predictions)
+        xml_update.write_classification_xml(
+            input_xml_path, output_xml_path,
+            pos_predictions, type_predictions,
+            staff_bb, avg_glyph_height, height)
 
         return True
